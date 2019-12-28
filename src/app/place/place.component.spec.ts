bc151d733f4190c7b71c7c8c459bd3070cf84839
georgetegral <georgetegral@gmail.com>
2019-12-27 21:09:47 -0600

    Creacion de tests en el componente place

--- src/app/place/place.component.spec.ts
+++ src/app/place/place.component.spec.ts
@@ -3,5 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlaceComponent } from './place.component';
 
 describe('PlaceComponent', () => {
-  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PlaceComponent]
+    });
+  });
+  it('should create the app', () => {
+    let fixture = TestBed.createComponent(PlaceComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  })
 });
