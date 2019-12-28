3d67969f29246f2b5fbc8d25e5e1863b2c0e30c4
georgetegral <georgetegral@gmail.com>
2019-12-27 20:53:09 -0600

    Se creo componente place para hacer tests nuevos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angularUnitTest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularUnitTest');
+    expect(app.title).toEqual('AngularUnitTest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularUnitTest app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularUnitTest app is running!');
   });
 });
